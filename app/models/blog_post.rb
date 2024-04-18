9f6e4e83cd4786ad3452bf3dddf9ccb485e726f9
francogaliotti <francogaliotti@gmail.com>
2024-04-18 17:16:07 -0300

    Second commit

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -3,4 +3,23 @@ class BlogPost < ApplicationRecord
     validates :body, presence: true
 
     belongs_to :user
+
+    scope :sorted, -> { order(published_at: :desc, updated_at: :desc )}
+    scope :draft, -> { where(published_at: nil) }
+    scope :published, -> { where("published_at <= ?", Time.current) }
+    scope :scheduled, -> { where("published_at > ?", Time.current) }
+    scope :get_mine, -> (user_id) { where(user_id: user_id) }
+
+    def draft?
+        published_at.nil?
+    end
+
+    def published?
+        published_at? and published_at <= Time.current
+    end
+
+    def scheduled?
+        published_at? and published_at > Time.current
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_17_140732) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_18_184918) do
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
+    t.datetime "published_at"
     t.index ["user_id"], name: "index_blog_posts_on_user_id"
   end
 
